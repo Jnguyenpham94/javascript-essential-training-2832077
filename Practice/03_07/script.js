322e8254b802f65fe6306ce628238102e01bfb45
Jnguyenpham94 <nguyenjonathan94@yahoo.com>
2023-03-07 19:36:15 -0800

    object practice DONE 03_07

--- Practice/03_07/script.js
+++ Practice/03_07/script.js
@@ -7,3 +7,34 @@
  * - Find an object that has another object inside of it to create a nested object.
  * - Test your objects in the browser console by accessing the entire object and its specific properties.
  */
+
+const dog = {
+  hair: true,
+  hairColor: "white",
+  age: 9,
+  hairLength: "medium",
+  cleanliness: true,
+  bath: function (bathed) {
+    this.cleanliness = bathed;
+  },
+};
+
+const person = {
+  fName: "Rick",
+  lName: "Sanchez",
+  iQ: 300,
+  hairColor: "gray",
+  clothes: false,
+  wearClothes: function (state) {
+    this.clothes = state;
+    if (this.clothes) {
+      console.log("He has clothes");
+    } else {
+      console.log("HE IS NAKED, Oh GEEZ");
+    }
+  },
+  armed: true,
+};
+
+console.log(dog);
+console.log("This person is: ", person);
